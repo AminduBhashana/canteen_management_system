aa1fe45b84c1465d6914350c5338eece7766f0e1
Chamath Kaushalya <chamathkaushalya8@gmail.com>
2024-06-05 13:14:11 +0530

    innitial commit

--- CanteenManagementSystem/Program.cs
+++ CanteenManagementSystem/Program.cs
@@ -16,7 +16,7 @@ namespace CanteenManagementSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            Application.Run(new frmMain());
         }
     }
 }
