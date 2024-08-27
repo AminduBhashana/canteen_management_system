3d5020f2f28a524475132c590a339d5b9aa3b03b
Chamath Kaushalya <chamathkaushalya8@gmail.com>
2024-08-27 09:25:23 +0530

    updated home screen

--- CanteenManagementSystem/frmMain.Designer.cs
+++ CanteenManagementSystem/frmMain.Designer.cs
@@ -383,9 +383,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(36, 65);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 23);
+            this.label2.Size = new System.Drawing.Size(70, 28);
             this.label2.TabIndex = 3;
             this.label2.Text = "label2";
             // 
